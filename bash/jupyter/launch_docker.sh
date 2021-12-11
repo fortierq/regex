2cc951900cb6cf08d02ac47b6231c44a23591948
Quentin Fortier <qpfortier@gmail.com>
2021-12-11 18:05:36 +0100

    Update launch_docker.sh

--- bash/jupyter/launch_docker.sh
+++ bash/jupyter/launch_docker.sh
@@ -1,1 +1,4 @@
-firefox $(docker logs mp2i_devcontainer_jupyter_1 2>&1 | grep -Eo "http://127.0.0.1[^ ]*" | head -n 1)
+export c=mp2i_devcontainer_jupyter_1; \
+docker container start $c; \
+while (( 1 - $(docker container ls | grep -c "$c") )); do sleep 1; done;
+firefox $(docker logs $c 2>&1 | grep -Eo "http://127.0.0.1[^ ]*token[^ ]*" | tail -n1)
