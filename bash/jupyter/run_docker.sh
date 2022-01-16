025c679a06166b33fce16bad7808d4fc5bdc84d3
Quentin Fortier <qpfortier@gmail.com>
2022-01-16 15:00:32 +0100

    Use mp2i-tex 3.0.0

--- bash/jupyter/run_docker.sh
+++ bash/jupyter/run_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export c=mp2i_devcontainer_jupyter_1; \
-docker container start $c; \
+export c=mp2i_jupyter;
+docker container start $c;
 while (( 1 - $(docker container ls | grep -c "$c") )); do sleep 1; done;
-firefox $(docker logs $c 2>&1 | grep -Eo "http://127.0.0.1[^ ]*token[^ ]*" | tail -n1)
+google-chrome $(docker logs $c 2>&1 | grep -Eo "http://127.0.0.1[^ ]*token[^ ]*" | tail -n1)
