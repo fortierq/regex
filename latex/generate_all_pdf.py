5a9fb2a44aeb33085f9b0ac940a23b3881b9a797
Quentin Fortier <qpfortier@gmail.com>
2022-07-10 17:48:16 +0000

    Check if exam

--- latex/generate_all_pdf.py
+++ latex/generate_all_pdf.py
@@ -18,19 +18,20 @@ P = Path("/workspace/src").resolve()
 for f in P.rglob("**/*.tex"):
     # print(f)
     os.chdir(f.parent)
-    if 'td' in f.name:
-        for i in [0, 1]:
-            input = "\input{" + f.name + "}"
-            output = f.stem
-            if i == 1:
-                input = "\PassOptionsToClass{cor}{exam}" + input
-                output += "_cor"
-            if not Path(output + ".pdf").exists():
-                cmd = f"lualatex -interaction nonstopmode -halt-on-error -file-line-error -shell-escape --jobname={output} '{input}'"
-                print(cmd)
-                subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
+    with f.open("r") as file:
+        if 'exam' in file.readline():
+            for i in [0, 1]:
+                input = "\input{" + f.name + "}"
+                output = f.stem
+                if i == 1:
+                    input = "\PassOptionsToClass{cor}{exam}" + input
+                    output += "_cor"
                 if not Path(output + ".pdf").exists():
-                    print(">"*10 + f" ERROR: {output} not generated")
+                    cmd = f"lualatex -interaction nonstopmode -halt-on-error -file-line-error -shell-escape --jobname={output} '{input}'"
+                    print(cmd)
+                    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
+                    if not Path(output + ".pdf").exists():
+                        print(">"*10 + f" ERROR: {output} not generated")
     os.chdir(str(P))
 
 extensions = ["aux", "log", "out", "toc", "vrb", "nav", "snm", "out", "pyg"]
