08dc44a071aa0eb7fa8dff1170b22d067793ede6
Quentin Fortier <qpfortier@gmail.com>
2023-02-15 18:52:48 +0100

    Updatr

--- cours/dl_nb.py
+++ cours/dl_nb.py
@@ -8,10 +8,10 @@ subprocess.run(["git", "clone", "--dep", "1", git, dir_repo])
 
 def get_dl(d):
     if isinstance(d, dict):
-        if "dl" in d:
-            yield d["dl"]
-            d["file"] = d["dl"]
-            del d["dl"]
+        if "tp" in d:
+            yield d["tp"]
+            d["file"] = d["tp"]
+            del d["tp"]
         for k, v in d.items():
             yield from get_dl(v)
     elif isinstance(d, list):
@@ -21,7 +21,11 @@ def get_dl(d):
 with Path("files/_toc.yml").open() as f:
     d = yaml.load(f, Loader=yaml.FullLoader)
     for dl in get_dl(d):
-        p = Path("files") / dl
+        p = Path("files") / tp
         p.parent.mkdir(parents=True, exist_ok=True)
         subprocess.run(["cp", (dir_repo / dl).absolute(), p])
+        subprocess.run(f"""jupyter nbconvert {p} --to ipynb --output {p} --allow-errors \
+        --TagRemovePreprocessor.enabled=True \
+        --TagRemovePreprocessor.remove_input_tags hide \
+        --TagRemovePreprocessor.remove_cell_tags cor""")
 yaml.dump(d, Path("files/_toc.yml").open("w"))
