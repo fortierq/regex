05ee93b4d060a80f43f386c29baaaf26609e9852
Quentin Fortier <qpfortier@gmail.com>
2023-02-15 17:36:38 +0100

    Update

--- cours/dl_nb.py
+++ cours/dl_nb.py
@@ -1,14 +1,9 @@
 from pathlib import Path
 import subprocess
-import sys
 import yaml
-import os
-
-dir_repo = Path("/tmp/cours-src")
-subprocess.run(["git", "config", "--global", "user.email", "qpfortier@gmail.com"])
-subprocess.run(["git", "config", "--global", "user.name", "Quentin Fortier"])
 
 git = f"https://github.com/fortierq/cours.git"
+dir_repo = Path("/tmp/cours-src")
 subprocess.run(["git", "clone", "--dep", "1", git, dir_repo])
 
 def get_dl(d):
@@ -24,23 +19,9 @@ def get_dl(d):
             yield from get_dl(v)
 
 with Path("files/_toc.yml").open() as f:
-    d = yaml.safe_load(f)
+    d = yaml.load(f, Loader=yaml.FullLoader)
     for dl in get_dl(d):
         p = Path("files") / dl
         p.parent.mkdir(parents=True, exist_ok=True)
         subprocess.run(["cp", (dir_repo / dl).absolute(), p])
 yaml.dump(d, Path("files/_toc.yml").open("w"))
-
-# for p in Path(dir).rglob("*.pdf"):
-#     if p.name not in private_files and "rc" not in p.parts:
-#         print(p)
-#         p_rel = p.relative_to(dir)
-#         p_rel.parent.mkdir(parents=True, exist_ok=True)
-#         subprocess.run(["cp", p.absolute(), p_rel])
-
-# subprocess.run(["git", "checkout", "--orphan", "new"])
-# subprocess.run(["git", "add", "--all"])
-# subprocess.run(["git", "commit", "-m", "Update", "-q"])
-# subprocess.run(["git", "branch", "-D", "main"])
-# subprocess.run(["git", "branch", "-m", "main"])
-# subprocess.run(["git", "push", "-f", git, "--set-upstream", "main"])
