ea6db77f4cf00ff5586ef8b97ec88792b08b745a
Quentin Fortier <qpfortier@gmail.com>
2023-02-17 15:57:41 +0100

    Update dl_nb.py

--- cours/dl_nb.py
+++ cours/dl_nb.py
@@ -15,7 +15,7 @@ def get_dl(d):
     elif isinstance(d, dict):
         for k in d.copy():
             if k in ["tp", "cor", "slides"]:
-                p = Path("files") / d[k]
+                p = Path("files") / "dl" / d[k]
                 p.parent.mkdir(parents=True, exist_ok=True)
                 subprocess.run(["cp", (dir_repo / d[k]).absolute(), p])
                 cmd = f"jupyter nbconvert {p} --to ipynb --output {p.name} --allow-errors --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_input_tags hide"
@@ -28,7 +28,10 @@ def get_dl(d):
                         nb["cells"][0]["source"][0] += '\n'
                         nb["cells"][0]["source"].append(f'<iframe src=https://mozilla.github.io/pdf.js/web/viewer.html?file=https://raw.githubusercontent.com/fortierq/cours/main/{Path(d[k]).with_suffix(".pdf")}#zoom=page-fit&pagemode=none height=500 width=100% allowfullscreen></iframe>')
                         json.dump(nb, p.open("w"))
-                d["file"] = d[k]
+                d["file"] = str(p.relative_to("files"))
+                subprocess.run(["git", "add", p])
+                subprocess.run(["git", "commit", "-m", f"Add {p.name}"])
+                subprocess.run(["git", "push"])
             if k in ["exercices", "cours"]:
                 p = (Path(f"files/{k}") / d[k]).with_suffix(".md")
                 p.parent.mkdir(parents=True, exist_ok=True)
